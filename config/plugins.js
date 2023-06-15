8d51d97e028e5aa7411ef5a0410f406c93731be2
lisandrosz <szyzuklisandro@gmail.com>
2023-06-15 19:26:39 -0300

    nuevo bucket, claves en .env

--- config/plugins.js
+++ config/plugins.js
@@ -3,11 +3,13 @@ module.exports = ({ env }) => ({
     config: {
       provider: "aws-s3",
       providerOptions: {
-        accessKeyId: "AKIA5QBZ6KBSDD3TWPMR",
-        secretAccessKey: "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",
-        region: "sa-east-1",
+        accessKeyId: env("AWS_ACCESS_KEY_ID"),
+        secretAccessKey: env("AWS_ACCESS_SECRET"),
+        region: env("AWS_REGION"),
         params: {
-          Bucket: "nippon-shop",
+          ACL: env("AWS_ACL", "public-read"),
+          signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 15 * 60),
+          Bucket: env("AWS_BUCKET"),
         },
       },
     },
